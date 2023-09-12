45833f856fa3dcae8796394c49834c7a191644c0
Robbie Stamp <stamprobbie@gmail.com>
2023-09-12 16:06:13 +0100

    adding mood to the tasks table

--- server/models/Task.js
+++ server/models/Task.js
@@ -5,6 +5,7 @@ class Task {
     this.id = data.id
     this.name = data.name
     this.description = data.description
+    this.mood = data.mood
     this.completed_at = data.completed_at
   }
 
@@ -17,13 +18,11 @@ class Task {
   }
 
   static async create(data) {
-    const{name, description, completed_at} = data;   
-    const response = await db.query('INSERT INTO tasks (name, description, completed_at) VALUES ($1, $2, $3) RETURNING *;', [name, description, completed_at]);
+    const{name, description, mood, completed_at} = data;   
+    const response = await db.query('INSERT INTO tasks (name, description, mood, completed_at) VALUES ($1, $2, $3, $4) RETURNING *;', [name, description, mood, completed_at]);
 
     return response.rows.map(t => new Task(t))
-}
-
-  
+  }
 }
 
 module.exports = Task
