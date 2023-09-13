d2a6d6456d428e8a36e509df2bf64a49c9aa5f33
Robbie Stamp <stamprobbie@gmail.com>
2023-09-13 15:46:41 +0100

    keeping branch up to date

--- server/__tests__/controllers/taskController.spec.js
+++ server/__tests__/controllers/taskController.spec.js
@@ -24,14 +24,6 @@ describe('task controller', () => {
       expect(mockStatus).toHaveBeenCalledWith(200)
   })
 
-  it('returns status 500 when things go wrong', async () => {
-    const errorMessage = 'Could not retrieve tasks'
-
-    Task.getAll = mockSend.mockRejectedValue(new Error(errorMessage))
-
-      await taskController.index(null, mockRes)
-      expect(mockStatus).toHaveBeenCalledWith(500)
-  })
 
   it('calls Task.getAll', async () => {
     const taskData = { id: 1, name: 'Wash Dishes', description: 'wash dishes after dinner', completed_at:'11:05PM-9/11/2023' }
@@ -43,6 +35,15 @@ describe('task controller', () => {
     expect(Task.getAll).toHaveBeenCalledTimes(1)
   })
 
+  it('returns status 500 when things go wrong', async () => {
+    const errorMessage = 'Could not retrieve tasks'
+
+    Task.getAll = mockSend.mockRejectedValue(new Error(errorMessage))
+
+      await taskController.index(null, mockRes)
+      expect(mockStatus).toHaveBeenCalledWith(500)
+  })
+  
   it('calls Task.create', async () => {
     const taskData = { id: 1, name: 'Wash Dishes', description: 'wash dishes after dinner', completed_at:'11:05PM-9/11/2023' }
 
