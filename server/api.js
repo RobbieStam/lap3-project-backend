076959664890f418341ed4b63aace89770274935
Franki Callard <francescalily@outlook.com>
2023-09-14 10:25:01 +0100

    functioning token backend

--- server/api.js
+++ server/api.js
@@ -4,12 +4,14 @@ const logger = require("morgan");
 
 const taskRouter = require("./routes/taskRoutes");
 const userRouter = require("./routes/user");
+//const postRouter = require("./routes/post");
 
 const api = express();
 
 api.use(cors());
 api.use(express.json());
 api.use(logger("dev"));
+const logRoutes = require("./middleware/logger");
 
 api.get("/", (req, res) => {
   res.json({
@@ -21,5 +23,6 @@ console.log({ taskRouter, userRouter });
 
 api.use("/tasks", taskRouter);
 api.use("/users", userRouter);
+api.use(logRoutes);
 
 module.exports = api;
